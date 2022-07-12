62ce942285eba4b65c5ed270ca73cdf8ea9436f4
sebin kwon <sbkwon98@gmail.com>
2022-07-13 01:43:23 +0900

    Update python, codeup

---
+++ Codeup/6044.py
@@ -0,0 +1,2 @@
+a, b = map(int,input().split())
+print(a+b,a-b,a*b,a//b,a%b,format(a/b, '.2f'), sep='\n')
---
+++ Codeup/6045.py
@@ -0,0 +1,3 @@
+a, b, c = map(int,input().split())
+avg = (a+b+c)/3
+print(a+b+c,format(avg, '.2f'))
