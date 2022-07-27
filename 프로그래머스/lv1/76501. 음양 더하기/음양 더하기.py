ca9df2c096cc475b7482b537fb326c834bcdd7aa
SeBin-Kwon <sbkwon98@gmail.com>
2022-07-27 14:17:36 +0900

    [level 1] Title: 음양 더하기, Time: 3.40 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,9 @@
+def solution(absolutes, signs):
+    answer = 0
+    for i in range(len(absolutes)):
+        if signs[i] == True:
+            continue
+        elif signs[i] == False:
+            absolutes[i] += absolutes[i] * -2
+            answer = sum(absolutes)
+    return answer
