c5183eb907cbd5ec6c9e6fc9c1be024205ac3677
SeBin-Kwon <sbkwon98@gmail.com>
2022-08-08 13:59:31 +0900

    [Bronze II] Title: 블랙잭, Time: 140 ms, Memory: 36188 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,13 @@
+n, m = map(int,input().split())
+num = list(map(int,input().split()))
+result = []
+
+for i in range(n-2):
+    for j in range(i+1,n-1):
+        for k in range(j+1,n):
+            sum_ = num[i] + num[j] + num[k]
+            if sum_ > m:
+                continue
+            else:
+                result.append(sum_)
+print(max(result))
