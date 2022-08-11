f8607f86d40b05ff2eb9f45597e95a7f57fb869d
SeBin-Kwon <sbkwon98@gmail.com>
2022-08-11 13:53:55 +0900

    [Bronze IV] Title: 와글와글 숭고한, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
+++ 백준/Bronze/17388. 와글와글 숭고한/와글와글 숭고한.py
@@ -1,10 +1,8 @@
-s,k,h = list(map(int,input().split()))
-if s+k+h >= 100:
+a = list(map(int,input().split()))
+b = ['Soongsil', 'Korea','Hanyang']
+
+if sum(a) >= 100:
     print('OK')
-elif s+k+h < 100:
-    if s < k and s < h:
-        print('Soongsil')
-    elif k < s and k < h:
-        print('Korea')
-    elif h < s and h < k:
-        print('Hanyang')
+
+else:
+    print(b[a.index(min(a))])
