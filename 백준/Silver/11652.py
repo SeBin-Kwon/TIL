67884037c4f010ab73eed61babd7ffe62c18ce38
sebin kwon <sbkwon98@gmail.com>
2022-08-12 22:26:55 +0900

    Update baekjoon

--- 백준/Silver/11652.py
+++ 백준/Silver/11652.py
@@ -1,16 +1,14 @@
-n = ''
-p = 0
+import sys
+sys.stdin = open('11652.txt', 'r')
 
-while n != '고무오리 디버깅 끝':
-    n = input()
-    if n == '문제':
-        p += 1
-    elif n == '고무오리':
-        if p != 0:
-            p -= 1
-        else:
-            p += 2
-if p:
-    print('힝구')
-else:
-    print('고무오리야 사랑해')
+n = int(input())
+dic = {}
+for _ in range(n):
+    m = int(input())
+    if m not in dic:
+        dic[m] = 1
+    else:
+        dic[m] += 1
+
+result = sorted(dic.items(), key = lambda x : (-x[1],x[0]))
+print(result[0][0])
