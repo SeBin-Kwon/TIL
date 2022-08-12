061f45d47623a0b1dbe13345c8679bd20680a4f8
SeBin-Kwon <sbkwon98@gmail.com>
2022-08-12 22:23:45 +0900

    [Silver IV] Title: 카드, Time: 3964 ms, Memory: 53908 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,11 @@
+n = int(input())
+dic = {}
+for _ in range(n):
+    m = int(input())
+    if m not in dic:
+        dic[m] = 1
+    else:
+        dic[m] += 1
+
+result = sorted(dic.items(), key = lambda x : (-x[1],x[0]))
+print(result[0][0])
