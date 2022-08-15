d80a5c6b5596be4281b2ccca03f3a4fbaa54cdff
SeBin-Kwon <sbkwon98@gmail.com>
2022-08-15 23:05:59 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 468 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.py
@@ -0,0 +1,26 @@
+while True:
+    n = input()
+    if n == '.':
+        break
+    g = []
+
+    for i in n:
+        if i == '(' or i == '[':
+            g.append(i)
+        elif i == ')':
+            if len(g) != 0 and g[-1] == '(':
+                g.pop()
+            else:
+                g.append(i)
+                break
+        elif i == ']':
+            if len(g) != 0 and g[-1] == '[':
+                g.pop()
+            else:
+                g.append(i)
+                break
+
+    if len(g) != 0:
+        print('no')
+    else:
+        print('yes')
