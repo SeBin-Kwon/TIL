721afe10c20df0aaccf7f2c0e8378cffbfa19876
SeBin-Kwon <sbkwon98@gmail.com>
2022-08-29 23:12:30 +0900

    [Silver II] Title: 스택 수열, Time: 4012 ms, Memory: 35552 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,27 @@
+n = int(input())
+flg = True
+stack = []
+o = []
+cnt = 1
+
+for _ in range(n):
+    num = int(input())
+   
+    while cnt <= num:
+        stack.append(cnt)
+        o.append('+')
+        cnt += 1
+
+
+    if stack[-1] == num:
+        stack.pop()
+        o.append('-')
+
+    else:
+        flg = False
+        break
+    
+if flg == False:
+    print('NO')
+else:
+    print(*o,sep='\n')
