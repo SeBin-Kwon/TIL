94512ff922b9a24f5797cb3242ca43adffa5cc87
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-03 16:40:38 +0900

    [Gold IV] Title: PPAP, Time: 440 ms, Memory: 38444 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -0,0 +1,12 @@
+s = input()
+l = []
+for i in range(len(s)):
+    l.append(s[i])
+    if l[-4:] == ['P','P','A','P']:
+        l.pop()
+        l.pop()
+        l.pop()
+if l == ['P','P','A','P'] or l == ['P']:
+    print('PPAP')
+else:
+    print('NP')
