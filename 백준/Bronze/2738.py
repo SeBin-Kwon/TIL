9dbdfe887919e6d119413d539bc5096991c9f9eb
sebin kwon <sbkwon98@gmail.com>
2022-09-04 23:58:55 +0900

    Update baekjoon

---
+++ 백준/Bronze/2738.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('2738.txt','r')
+
+n, m = map(int,input().split())
+
+a = []
+for _ in range(n):
+    row = list(map(int,input().split()))
+    a.append(row)
+
+b = []
+for _ in range(n):
+    row = list(map(int,input().split()))
+    b.append(row)
+
+for i in range(n):
+    sum_ = []
+    for j in range(m):
+        sum_.append(a[i][j] + b[i][j])
+    print(*sum_)
+    
