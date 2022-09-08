f021b76acb79faa8e5e63fe2dcd9329e62de1716
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-08 23:58:30 +0900

    [Silver V] Title: 단어 정렬, Time: 104 ms, Memory: 35452 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+lst = []
+
+for i in range(n):
+    lst.append(sys.stdin.readline().strip())
+set_lst = set(lst)
+lst = list(set_lst)
+lst.sort()
+lst.sort(key = len)
+
+for i in lst:
+    print(i)
