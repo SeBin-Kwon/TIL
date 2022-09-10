6f73ce002b6983f9b7569901c80358fc984b70e3
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-10 19:57:25 +0900

    [Silver IV] Title: 수 찾기, Time: 192 ms, Memory: 50092 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+a = set(map(int,input().split()))
+m = int(input())
+b = list(map(int,input().split()))
+
+
+
+for i in b:
+    if i in a:
+        print(1)
+    else:
+        print(0)
