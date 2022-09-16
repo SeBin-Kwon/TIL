af3aef66ee9d614c258d739fde1af5582ba9d834
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-16 22:09:25 +0900

    [Silver II] Title: 좌표 압축, Time: 1896 ms, Memory: 145280 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,16 @@
+n = int(input())
+nums = list(map(int,input().split()))
+
+s_nums = sorted(set(nums))
+
+cnt = 0
+dic = {}
+for i in s_nums:
+    dic[i] = cnt
+    cnt += 1
+
+result = []
+for i in range(n):
+    result.append(dic[nums[i]])
+
+print(*result)
