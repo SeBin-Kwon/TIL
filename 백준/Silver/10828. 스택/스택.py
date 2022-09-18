d4734c6529df88a73ab86ebe7d0e3a349e76e827
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-18 16:16:24 +0900

    [Silver IV] Title: 스택, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+stack = []
+for i in range(n):
+    cmd = input().split()
+    if cmd[0] == 'push' :
+        stack.append(cmd[-1])
+    elif cmd[0] == 'pop':
+        if stack:
+            print(stack.pop())
+        else:
+            print(-1)
+    elif cmd[0] == 'size':
+        print(len(stack))
+    elif cmd[0] == 'empty':
+        if stack:
+            print(0)
+        else:
+            print(1)
+    elif cmd[0] == 'top':
+        if stack:
+            print(stack[-1])
+        else:
+            print(-1)
