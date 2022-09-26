02b7e40979020150dc01f22eecd4fe23dc409b2e
sebin kwon <sbkwon98@gmail.com>
2022-09-26 23:56:13 +0900

    Update baekjoon

---
+++ 백준/Silver/1927.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('1927.txt','r')
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+heap = []
+for i in range(n):
+    x = int(input())
+    if x != 0:
+        heapq.heappush(heap, x)
+    else:
+        if heap:
+            print(heapq.heappop(heap))
+        else:
+            print(0)
