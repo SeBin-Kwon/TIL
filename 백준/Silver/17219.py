e2ed60f64f79c8ff48895be94e5a622c4ac39cea
sebin kwon <sbkwon98@gmail.com>
2022-09-28 00:43:04 +0900

    Update Baekjoon, Add CS, Delete django settings.py

---
+++ 백준/Silver/17219.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('17219.txt', 'r')
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+dic = {}
+for i in range(n):
+    site, pw = input().split()
+    dic[site] = pw
+
+for j in range(m):
+    memo = input().strip('\n')
+    print(dic[memo])
+
