551b14b79b701f6a8916ed064c6192bc6ede8cb3
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-28 22:22:22 +0900

    [Silver III] Title: 수강신청, Time: 488 ms, Memory: 77044 KB -BaekjoonHub

---
+++ 백준/Silver/13414. 수강신청/수강신청.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+K, L = map(int,input().split())
+dic = {}
+for i in range(L):
+    dic[input().strip('\n')] = i
+result = sorted(dic.items(), key=lambda x : x[1])
+for i in result[:K]:
+    print(i[0])
