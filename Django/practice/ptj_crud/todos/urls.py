4b928224958e970dd7b718068b584d03d616309d
sebin kwon <sbkwon98@gmail.com>
2022-09-29 15:51:08 +0900

    Update ptj-crud modal

--- Django/practice/ptj_crud/todos/urls.py
+++ Django/practice/ptj_crud/todos/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('create/', views.create, name='create'),
     path('delete/<int:pk>', views.delete, name='delete'),
     path('update/<int:pk>', views.update, name='update'),
+    path('edit/<int:pk>', views.edit, name='edit'),
+    path('editUpdate/<int:pk>', views.editUpdate, name='editUpdate'),
 ]
 
--- Django/practice/ptj_crud/todos/views.py
+++ Django/practice/ptj_crud/todos/views.py
@@ -3,10 +3,10 @@ from .models import Todo
 
 # Create your views here.
 def index(request):
-    todos = Todo.objects.all()
+    todos = Todo.objects.all().order_by('priority')
 
     context = {
-        'todos' : todos
+        'todos' : todos,
     }
     return render(request, 'todos/index.html', context)
 
@@ -14,6 +14,7 @@ def create(request):
     content = request.GET.get('content')
     priority = request.GET.get('priority')
     deadline = request.GET.get('deadline')
+
     Todo.objects.create(content=content,priority=priority,deadline=deadline)
     
     return redirect('todos:index')
@@ -25,5 +26,20 @@ def delete(request,pk):
 def update(request,pk):
     todos = Todo.objects.get(id=pk)
     todos.completed = not todos.completed
+    todos.save()
+    return redirect('todos:index')
+
+def edit(request,pk):
+    todos = Todo.objects.get(id=pk)
+    context = {
+        'todos' : todos
+    }
+    return redirect('todos:index', context)
+
+def editUpdate(request,pk):
+    todos = Todo.objects.get(id=pk)
+    todos.content = request.GET.get('content')
+    todos.priority = request.GET.get('priority')
+    todos.deadline = request.GET.get('deadline')
     todos.save()
     return redirect('todos:index')
