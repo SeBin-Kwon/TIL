508df8039105475e0f4f260f93e0c9dfb3d7d687
SeBin-Kwon <sbkwon98@gmail.com>
2022-09-29 22:34:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+def dp(n):
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    elif n == 3:
+        return 4
+    else:
+        return dp(n-1) + dp(n-2) + dp(n-3)
+
+
+T = int(input())
+for t in range(T):
+
+    n = int(input())
+    print(dp(n))
