6ca4bf612ba54b01053af94aa483b1baf0201df9
SeBin-Kwon <sbkwon98@gmail.com>
2022-10-02 18:48:11 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -1,15 +1,11 @@
 import sys
 input = sys.stdin.readline
 
+s = [0, 1, 3]
+
+for i in range(3, 1001):
+  s.append((s[i - 2] * 2) + s[i - 1])
+  
 n = int(input())
-dp = [0] * 1001
 
-# 초기값 지정
-dp[0] = 1
-dp[1] = 1
-
-# 점화식에 따른 경우의 수 계산
-for i in range(2, n+1):
-    dp[i] = dp[i-1] + 2 * dp[i-2]
-
-print(dp[n]%10007)
+print(s[n] % 10007)
