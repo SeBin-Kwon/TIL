02d4f8d61969844d456e798be5afa4c6b754d26b
sebin kwon <sbkwon98@gmail.com>
2022-10-04 18:05:20 +0900

    Update CS, Baekjoon

---
+++ 백준/Silver/11047.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('11047.txt', 'r')
+input = sys.stdin.readline
+
+n, k = map(int,input().split())
+
+coins = []
+for i in range(n):
+    coin = int(input())
+    if coin <= k:
+        coins.append(coin)
+cnt = 0
+for i in coins[::-1]:
+    cnt += k // i
+    k = k % i
+    if k == 0:
+        break
+print(cnt)
+        
+
+
+
+
