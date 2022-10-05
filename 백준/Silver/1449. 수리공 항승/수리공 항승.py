925746a307dd1e320bec8640657cfa9b244c2fbc
SeBin-Kwon <sbkwon98@gmail.com>
2022-10-06 00:14:19 +0900

    [Silver III] Title: 수리공 항승, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1449. 수리공 항승/수리공 항승.py
+++ 백준/Silver/1449. 수리공 항승/수리공 항승.py
@@ -6,14 +6,13 @@ pipe = list(map(int,input().split()))
 pipe.sort()
 
 cnt = 1
-start = pipe[0]
+
 end = pipe[0]+l
 
 for i in range(n):
-    if start <= pipe[i] < end:
+    if pipe[i] < end:
         continue
     else:
-        start = pipe[i]
         end = pipe[i]+l
         cnt += 1
 print(cnt)
