92608886d3bb4c4fe140ea73216a590103cd1fc2
sebin kwon <sbkwon98@gmail.com>
2022-10-10 02:22:10 +0900

    Update baekjoon

--- 백준/Bronze/15829.py
+++ 백준/Bronze/15829.py
@@ -2,3 +2,9 @@ import sys
 sys.stdin = open('15829.txt', 'r')
 input = sys.stdin.readline
 
+l = int(input())
+word = input()
+sum_ = 0
+for i in range(l):
+    sum_ += (ord(word[i])-96)*(31**i)
+print(sum_ % 1234567891)
