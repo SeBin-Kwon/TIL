613ea5fd30886d3b61df0f90a14946efe9d16d26
SeBin-Kwon <sbkwon98@gmail.com>
2022-11-07 00:05:55 +0900

    [Silver II] Title: 통계학, Time: 868 ms, Memory: 119712 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -9,30 +9,31 @@ nums.sort()
 print(round(sum(nums) / n))
 print(nums[n // 2])
 
-counts = dict()
-for i in range(1,n+1) :
-	counts[i] = []
+mode = dict()
+for i in range(1,n+1):
+	mode[i] = []
 
-maxCount = 1
-count = 1
-for j in range(1,n) :
-	if nums[j] == nums[j-1] :
-		count += 1
-	else :
-		counts[count].append(nums[j-1])
-		if maxCount < count : maxCount = count
-		count = 1
-	if j == n-1 : 
-		counts[count].append(nums[j])
-		if maxCount < count : maxCount = count
+maxcnt = 1
+cnt = 1
+for i in range(1,n):
+    if nums[i] == nums[i-1]:
+        cnt += 1
+    else:
+        mode[cnt].append(nums[i-1])
+        if maxcnt < cnt:
+            maxcnt = cnt
+        cnt = 1
+    if i == n-1: 
+        mode[cnt].append(nums[i])
+        if maxcnt < cnt:
+            maxcnt = cnt
+if n == 1:
+    mode[1].append(nums[0])
 
-if n == 1 :
-	counts[1].append(nums[0])
-
-counts[maxCount].sort()
-if len(counts[maxCount]) == 1 :
-	print(counts[maxCount][0])
+mode[maxcnt].sort()
+if len(mode[maxcnt]) == 1:
+    print(mode[maxcnt][0])
 else :
-	print(counts[maxCount][1])
+	print(mode[maxcnt][1])
 
 print(nums[-1] - nums[0])
