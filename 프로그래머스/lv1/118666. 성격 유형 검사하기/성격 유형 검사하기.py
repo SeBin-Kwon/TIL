f2056fc0ebc95d00376aff068518ea7703f51c2f
strong_bean <sbkwon98@gmail.com>
2022-12-03 18:58:01 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.39 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/118666. 성격 유형 검사하기/성격 유형 검사하기.py
@@ -0,0 +1,27 @@
+def solution(survey, choices):
+    answer = ''
+    dic = {'R' : 0, 'T' : 0, 'C' : 0, 'F' : 0, 'J' : 0, 'M' : 0, 'A' : 0, 'N' : 0 }
+    for i in range(len(survey)):
+        score =  abs(choices[i] - 4)
+        if choices[i] < 4:
+            dic[survey[i][0]] += score
+        elif choices[i] > 4:
+            dic[survey[i][1]] += score
+    if dic['R'] >= dic['T']:
+        answer += 'R'
+    else:
+        answer += 'T'
+    if dic['C'] >= dic['F']:
+        answer += 'C'
+    else:
+        answer += 'F'
+    if dic['J'] >= dic['M']:
+        answer += 'J'
+    else:
+        answer += 'M'
+    if dic['A'] >= dic['N']:
+        answer += 'A'
+    else:
+        answer += 'N'
+
+    return answer
