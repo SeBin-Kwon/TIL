900a515c59fc0b0f9ec904ae5b802e733ada2aa0
strong_bean <sbkwon98@gmail.com>
2022-12-28 20:42:33 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1808 ms, Memory: 101204 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, k, x = map(int, input().split())
+city = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    city[a].append(b)
+visited = [0] * (n+1)
+d = [0] * (n+1)
+result = []
+
+def bfs(start):
+    queue = deque([start])
+    visited[start] = 1
+    while queue:
+        node = queue.popleft()
+        for i in city[node]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = 1
+                d[i] = d[node] + 1
+                if d[i] == k:
+                    result.append(i)
+    if result:
+        result.sort()
+        print(*result,sep='\n')
+    else:
+        print(-1)
+
+bfs(x)
