174954ef66273f9b9192c21a9ef998d1a8c4e74a
strong_bean <sbkwon98@gmail.com>
2023-01-03 16:37:44 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -2,17 +2,15 @@ import sys
 
 input = sys.stdin.readline
 
-zero = [1, 0, 1]
-one = [0, 1, 1]
+dp = [0] * 41
+dp[0] = (1, 0)
+dp[1] = (0, 1)
+dp[2] = (1, 1)
 
-def fibonacci(n):
-    length = len(zero)
-    if n >= length:
-        for i in range(length, n+1):
-            zero.append(zero[i-1] + zero[i-2])
-            one.append(one[i-1] + one[i-2])
-    print(zero[n], one[n])
+for i in range(3, 41):
+    dp[i] = (dp[i-1][1], sum(dp[i-1]))
+
 T = int(input())
 for t in range(T):
     n = int(input())
-    fibonacci(n)
+    print(*dp[n])
