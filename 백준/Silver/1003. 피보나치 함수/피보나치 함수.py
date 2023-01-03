84d33872b6ea0da655af549564c280c671383f04
strong_bean <sbkwon98@gmail.com>
2023-01-03 16:40:29 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -2,15 +2,10 @@ import sys
 
 input = sys.stdin.readline
 
-dp = [0] * 41
-dp[0] = (1, 0)
-dp[1] = (0, 1)
-dp[2] = (1, 1)
-
-for i in range(3, 41):
-    dp[i] = (dp[i-1][1], sum(dp[i-1]))
-
 T = int(input())
 for t in range(T):
     n = int(input())
-    print(*dp[n])
+    zero, one = 1, 0
+    for i in range(n):
+        zero, one = one, zero + one
+    print(zero, one)
