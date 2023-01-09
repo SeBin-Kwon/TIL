00a54d47c2ddbdcde958ebb452034c9812f67664
strong_bean <sbkwon98@gmail.com>
2023-01-09 23:40:40 +0700

    [Silver V] Title: 다리 놓기, Time: 76 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -0,0 +1,11 @@
+def factorial(n):
+    num = 1
+    for i in range(1, n+1):
+        num *= i
+    return num
+
+T = int(input())
+for t in range(T):
+    n, m = map(int,input().split())
+    bridge = factorial(m) // (factorial(n) * factorial(m - n))
+    print(bridge)
