d6a00f8de788e9ecb41759ed8c8a303efd7ce157
strong_bean <sbkwon98@gmail.com>
2023-01-24 00:07:33 +0900

    [Silver V] Title: 소수, Time: 540 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/2581. 소수/소수.py
+++ 백준/Silver/2581. 소수/소수.py
@@ -1,19 +1,17 @@
-start_num = int(input())
-last_num = int(input())
-
-sosu_list = []
-for num in range(start_num, last_num+1):
-    error = 0
-    if num > 1 :
-        for i in range(2, num):  # 2부터 num-1까지
+start = int(input())
+end = int(input())
+answer = []
+for num in range(start,end+1):
+    flag = 0
+    if num != 1:
+        for i in range(2, num):
             if num % i == 0:
-                error += 1
-                break  # 2부터 num-1까지 나눈 몫이 0이면 error가 증가하고 for문을 끝냄
-        if error == 0:
-            sosu_list.append(num)  # error가 없으면 소수리스트에 추가
-            
-if len(sosu_list) > 0 :
-    print(sum(sosu_list))
-    print(min(sosu_list))
+                flag += 1
+                break
+        if flag == 0:
+            answer.append(num)
+if answer:
+    print(sum(answer))
+    print(min(answer))
 else:
     print(-1)
