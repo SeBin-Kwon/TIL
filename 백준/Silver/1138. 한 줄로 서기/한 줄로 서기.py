b21e7277e89213e051557ed3b7688c6e3f08b043
strong_bean <sbkwon98@gmail.com>
2023-01-25 17:41:12 +0900

    [Silver II] Title: 한 줄로 서기, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.py
@@ -2,15 +2,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-people = list(map(int,input().split()))
-answer = [0] * n
-
-for i in range(n):
-    cnt = 0
-    for j in range(n):
-        if people[i] == cnt and answer[j] == 0:
-            answer[j] = i + 1
-            break
-        elif answer[j] == 0:
-            cnt += 1
+people = list(map(int,input().split()))[::-1]
+answer = []
+for i in people:
+    answer.insert(i,n)
+    n -= 1
 print(*answer)
