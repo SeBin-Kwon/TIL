b62fa437076aa3e0944ab79247fe39e539d5f3c4
strong_bean <sbkwon98@gmail.com>
2023-02-04 17:47:35 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -0,0 +1,21 @@
+import sys
+from itertools import combinations
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+for i in range(n):
+    s, b = map(int, input().split())
+    arr.append((s,b))
+
+result = int(1e9)
+for i in range(1,n+1):
+    for j in combinations(arr,i):
+        s = 1
+        b = 0
+        for k in j:
+            s *= k[0]
+            b += k[1]
+            if abs(s - b) < result:
+                result = abs(s - b)
+print(result)
