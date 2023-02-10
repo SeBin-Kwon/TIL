716ffbaf7a0f0cdd59292b9ec03600bd6b1323b1
strong_bean <sbkwon98@gmail.com>
2023-02-10 14:58:35 +0900

    [Silver I] Title: 케빈 베이컨의 6단계 법칙, Time: 64 ms, Memory: 34128 KB -BaekjoonHub

---
+++ 백준/Silver/1389. 케빈 베이컨의 6단계 법칙/케빈 베이컨의 6단계 법칙.py
@@ -0,0 +1,28 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def bfs(start):
+    visited = [-1] * (n+1)
+    queue = deque([start])
+    visited[start] = 0
+    while queue:
+        node = queue.popleft()
+        for i in friend[node]:
+            if visited[i] == -1:
+                queue.append(i)
+                visited[i] = visited[node] + 1
+    return sum(visited) + 1
+
+n, m = map(int, input().split())
+friend = [[] for _ in range(n+1)]
+for i in range(m):
+    a, b = map(int, input().split())
+    friend[a].append(b)
+    friend[b].append(a)
+
+answer = []
+for i in range(1,n+1):
+    answer.append(bfs(i))
+
+print(answer.index(min(answer)) + 1)
