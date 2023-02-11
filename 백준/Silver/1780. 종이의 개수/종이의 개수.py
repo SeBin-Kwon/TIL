6b0aa1f918ca83cc9f0c7b4c652b8239c10771c7
strong_bean <sbkwon98@gmail.com>
2023-02-11 17:11:22 +0900

    [Silver II] Title: 종이의 개수, Time: 832 ms, Memory: 163260 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -5,24 +5,21 @@ n = int(input())
 paper = []
 for _ in range(n):
     paper.append(list(map(int,input().split())))
+result = [0, 0, 0]
 
 def divide(x, y, n):
-    global minus, zero, plus
     check = paper[x][y]
+    if n == 1:
+        result[check + 1] += 1
+        return
     for i in range(x, x + n):
         for j in range(y, y + n):
             if check != paper[i][j]:
-                for k in range(3):
-                    for l in range(3):
-                        divide(x + k * n // 3, y + l * n // 3, n // 3)
+                for k in range(x, x + n, n // 3):
+                    for l in range(y, y + n, n // 3):
+                        divide(k, l, n // 3)
                 return
-    if check == -1:
-        minus += 1
-    elif check == 0:
-        zero += 1
-    else:
-        plus += 1
+    result[check + 1] += 1
 
-minus, zero, plus = 0, 0, 0
 divide(0, 0, n)
-print(minus, zero, plus, sep="\n")
+print(*result, sep="\n")
