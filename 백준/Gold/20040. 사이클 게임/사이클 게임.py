f0a73a626b5703a3ee440349bba93e1facff6e7d
strong_bean <sbkwon98@gmail.com>
2023-02-22 23:15:20 +0900

    [Gold IV] Title: 사이클 게임, Time: 1796 ms, Memory: 50684 KB -BaekjoonHub

---
+++ 백준/Gold/20040. 사이클 게임/사이클 게임.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+parents = [i for i in range(n)] #[0,1,2,3,4,5]
+endgame = 0
+
+def find(x):
+    # x가 root가 아니라면 재귀로 root를 다시 찾고 갱신한다.
+    if x != parents[x]:
+        parents[x] = find(parents[x])
+        return parents[x]
+	# x가 root라면 그대로 반환
+    if x == parents[x]:
+        return x
+
+def union(x, y, i):
+    global endgame
+    # root 찾기
+    x = find(x)
+    y = find(y)
+    # 서로 다른 그룹이라면, 사이클이 안 만들어졌다면
+    if x != y:
+        # 트리를 합친다.
+        parents[max(x,y)] = min(x,y)
+    elif endgame == 0:
+        endgame = i
+
+for i in range(m):
+    a, b = map(int, input().split())
+    union(a, b, i + 1)
+
+print(endgame)
