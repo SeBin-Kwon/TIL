c2f3b83134957443642656d372e5b6f989ee631e
sebin kwon <sbkwon98@gmail.com>
2023-03-27 23:29:03 +0900

    Update Baekjoon

--- 백준/Gold/16928.py
+++ 백준/Gold/16928.py
@@ -1,30 +1,38 @@
 import sys
-sys.stdin = open('16928.txt', 'r')
+sys.stdin = open('16928.txt','r')
+from collections import deque
 input = sys.stdin.readline
 
-# 주사위 1 ~ 6
-# 게임 10 x 10, 100개의 칸
-# 주사위 굴린 결과 > 100은 안됨
-# 도착한 칸 : 사다리 => 앞, 뱀 => 뒤
-# 도착한 칸 == 100이 되기 위해 주사위 굴리는 최소 횟수
-# n : 사다리의 수, m : 뱀의 수 
+def bfs():
+    queue = deque([1])
+    visited[1] = 1
+    while queue:
+        current = queue.popleft()
+        
+        for i in range(1, 7):
+            next = current + i
+            if next < 101:
+                if not visited[next]:
+                    board[next] = board[current] + 1
+                    if move[next]:
+                        if not visited[move[next]]:
+                            visited[move[next]] = 1
+                            board[move[next]] = board[next]
+                            queue.append(move[next])
+                            if move[next] == 100:
+                                return board[100]
+                    else:
+                        visited[next] = 1
+                        queue.append(next)
+                        if next == 100:
+                            return board[100]
 
 n, m = map(int, input().split())
-ladder = []
-for _ in range(n):
-    ladder.append(tuple(map(int, input().split())))
-snake = []
-for _ in range(m):
-    snake.append(tuple(map(int, input().split())))
+board = [0] * 101
+visited = [0] * 101
+move = [0] * 101
+for _ in range(n+m):
+    u, v = map(int, input().split())
+    move[u] = v
 
-ladder.sort()
-cnt = 0
-board = 0
-for i in range(1, 7):
-    if i != ladder[1]:
-        board += i
-print(ladder)
-# 주사위를 사다리나 뱀 나올 때 까지 굴려 1 ~ 6
-# 나오면 횟수 카운트 더해주기
-# 사다리와 뱀들이 연결되어 있다면 bfs
-# bfs 끝나면 횟수 카운트
+print(bfs())
--- 백준/Silver/5525.py
+++ 백준/Silver/5525.py
@@ -2,3 +2,19 @@ import sys
 sys.stdin = open('5525.txt', 'r')
 input = sys.stdin.readline
 
+n = int(input())
+m = int(input())
+s = input()
+answer, i, cnt = 0, 0, 0
+
+while i < m - 1:
+    if s[i:i+3] == 'IOI':
+        i += 2
+        cnt += 1
+        if cnt == n:
+            answer += 1
+            cnt -= 1
+    else:
+        i += 1
+        cnt = 0
+print(answer)
