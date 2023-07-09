45a63a79038c24c152a4cce386e4c1e76f8b226e
strong_bean <sbkwon98@gmail.com>
2023-07-09 20:45:44 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -2,6 +2,4 @@ from itertools import combinations
 n, m = map(int, input().split())
 nums = [i for i in range(1,n+1)]
 for items in combinations(nums, m):
-    for j in items:
-        print(j, end=' ')
-    print()
+    print(*items)
