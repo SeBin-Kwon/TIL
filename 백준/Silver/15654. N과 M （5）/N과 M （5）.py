5d41722b5282e7894f6562d60f6e9d0b9673ec1f
strong_bean <sbkwon98@gmail.com>
2023-07-11 10:06:42 +0900

    [Silver III] Title: N과 M (5), Time: 48 ms, Memory: 37192 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.py
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -5,5 +5,4 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 nums = sorted(list(map(int, input().split())))
 
-for i in permutations(nums, m):
-    print(*i)
+print('\n'.join(map(' '.join, permutations(map(str, nums), m))))
