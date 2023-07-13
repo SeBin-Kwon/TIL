887239fd75ce1fa6ad0bffacfa0a2ecacdb14d9c
strong_bean <sbkwon98@gmail.com>
2023-07-13 23:38:52 +0900

    [Silver IV] Title: solved.ac, Time: 152 ms, Memory: 36008 KB -BaekjoonHub

---
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+def my_round(x):
+    if x - int(x) >= 0.5:
+        return int(x) + 1
+    else:
+        return int(x)
+
+n = int(input())
+if n:
+    comments = [int(input()) for _ in range(n)]
+    comments.sort()
+    remove_num = my_round(n * 0.15)
+    print(my_round(sum(comments[remove_num:-remove_num] if remove_num else comments) / (n - 2 * remove_num)))
+else:
+    print(0)
