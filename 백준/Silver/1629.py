430bf85a743491377c60e3f665255ebe21f78621
SeBin-Kwon <sbkwon98@gmail.com>
2023-09-06 20:15:43 +0900

    Update baekjoon

---
+++ 백준/Silver/1629.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('1629.txt', 'r')
+input = sys.stdin.readline
+
+a, b, c = map(int, input().split())
+
+def multi(a,b):
+    if b == 1:
+        return a%c
+    else:
+        tmp = multi(a,b//2)
+        if b % 2 == 0:
+            return (tmp * tmp) % c
+        else:
+            return (tmp * tmp * a) % c
+print(multi(a,b))
+
---
+++ 백준/Silver/9465.py
@@ -0,0 +1,19 @@
+import sys
+sys.stdin = open('9465.txt', 'r')
+input = sys.stdin.readline
+
+T = int(input())
+for t in range(T):
+    n = int(input())
+    stickers = [list(map(int,input().split())) for _ in range(2)]
+    sum_ = 0
+    if n > 1:
+        # 1번 경로
+        stickers[0][1] += stickers[1][0]
+        # 2번 경로
+        stickers[1][1] += stickers[0][0]
+
+    for i in range(2,n):
+        stickers[0][i] += max(stickers[1][i-1], stickers[1][i-2])
+        stickers[1][i] += max(stickers[0][i-1], stickers[0][i-2])
+    print(max(stickers[0][n-1],stickers[1][n-1]))
