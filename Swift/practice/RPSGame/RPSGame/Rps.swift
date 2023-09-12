769f318f8496185a02bbf78c3c16e2f26be3c1c9
SeBin-Kwon <sbkwon98@gmail.com>
2023-09-13 00:53:50 +0900

    Update RPSGame

---
+++ Swift/practice/RPSGame/RPSGame/Rps.swift
@@ -0,0 +1,14 @@
+//
+//  Rps.swift
+//  RPSGame
+//
+//  Created by Sebin Kwon on 2023/09/13.
+//
+
+import Foundation
+
+enum Rps: Int {
+    case rock
+    case paper
+    case scissors
+}
--- Swift/practice/RPSGame/RPSGame/ViewController.swift
+++ Swift/practice/RPSGame/RPSGame/ViewController.swift
@@ -9,11 +9,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var myImageView: UIImageView!
+    
+    
+    @IBOutlet weak var comChoiceLabel: UILabel!
+    @IBOutlet weak var myChoiceLabel: UILabel!
+    
+    var myChoice: Rps = Rps.rock
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        comImageView.image = UIImage(named: "ready.png")
+        myImageView.image = UIImage(named: "ready.png")
+        
+        comChoiceLabel.text = "준비"
+        myChoiceLabel.text = "준비"
     }
 
-
+    @IBAction func rpsButtonTapped(_ sender: UIButton) {
+//        guard let title = sender.currentTitle else {
+//            return
+//        }
+        let title = sender.currentTitle!
+        switch title {
+        case "가위":
+            myChoice = Rps.scissors
+        case "바위":
+            myChoice = Rps.rock
+        case "보":
+            myChoice = Rps.paper
+        default:
+            break
+        }
+        
+    }
+    
+    @IBAction func selectButtonTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+    }
+    
 }
-
