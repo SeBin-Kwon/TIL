fa426dec58a54c7e8f5e9c27b63c9f623830ba67
SeBin-Kwon <sbkwon98@gmail.com>
2023-09-17 23:46:12 +0900

    Add select func and reset func

--- Swift/practice/RPSGame/RPSGame/ViewController.swift
+++ Swift/practice/RPSGame/RPSGame/ViewController.swift
@@ -18,7 +18,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var comChoiceLabel: UILabel!
     @IBOutlet weak var myChoiceLabel: UILabel!
     
+    var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
     var myChoice: Rps = Rps.rock
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,9 +50,54 @@ class ViewController: UIViewController {
     }
     
     @IBAction func selectButtonTapped(_ sender: UIButton) {
+        
+        switch comChoice {
+        case .rock:
+            comImageView.image = UIImage(named: "rock.png")
+            comChoiceLabel.text = "바위"
+        case .paper:
+            comImageView.image = UIImage(named: "paper.png")
+            comChoiceLabel.text = "바위"
+        case .scissors:
+            comImageView.image = UIImage(named: "scissors.png")
+            comChoiceLabel.text = "가위"
+        }
+        
+        switch myChoice {
+        case .rock:
+            myImageView.image = UIImage(named: "rock.png")
+            myChoiceLabel.text = "바위"
+        case .paper:
+            myImageView.image = UIImage(named: "paper.png")
+            myChoiceLabel.text = "바위"
+        case .scissors:
+            myImageView.image = UIImage(named: "scissors.png")
+            myChoiceLabel.text = "가위"
+        }
+        
+        if comChoice == myChoice {
+            mainLabel.text = "비겼다"
+        } else if comChoice == .rock && myChoice == .paper {
+            mainLabel.text = "이겼다"
+        } else if comChoice == .paper && myChoice == .scissors {
+            mainLabel.text = "이겼다"
+        } else if comChoice == .scissors && myChoice == .rock {
+            mainLabel.text = "이겼다"
+        } else {
+            mainLabel.text = "졌다"
+        }
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
+        comImageView.image = UIImage(named: "ready.png")
+        comChoiceLabel.text = "준비"
+        
+        myImageView.image = UIImage(named: "ready.png")
+        myChoiceLabel.text = "준비"
+        
+        mainLabel.text = "선택하세요"
+        
+        comChoice = Rps(rawValue: Int.random(in: 0...2))!
     }
     
 }
