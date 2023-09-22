f80aad1e260645446097083cc195328ac360a9b4
SeBin-Kwon <sbkwon98@gmail.com>
2023-09-22 20:19:56 +0900

    Add QuestionView

---
+++ Swift/practice/PDFKitPractice/PDFKitPractice/QuestionView.swift
@@ -0,0 +1,20 @@
+//
+//  QuestionView.swift
+//  PDFKitPractice
+//
+//  Created by Sebin Kwon on 2023/09/22.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
