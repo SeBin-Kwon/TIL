304d96e4d63829fd9a97f8cf4b34d34e837171ca
SeBin-Kwon <sbkwon98@gmail.com>
2023-09-28 23:19:46 +0900

    Add DynamicTypePractice

---
+++ SwiftUI/practice/DynamicTypePractice/DynamicTypePractice/DotsView.swift
@@ -0,0 +1,20 @@
+//
+//  DotsView.swift
+//  DynamicTypePractice
+//
+//  Created by Sebin Kwon on 2023/09/26.
+//
+
+import SwiftUI
+
+struct DotsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DotsView_Previews: PreviewProvider {
+    static var previews: some View {
+        DotsView()
+    }
+}
---
+++ SwiftUI/practice/DynamicTypePractice/DynamicTypePractice/DynamicTypePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DynamicTypePracticeApp.swift
+//  DynamicTypePractice
+//
+//  Created by Sebin Kwon on 2023/09/26.
+//
+
+import SwiftUI
+
+@main
+struct DynamicTypePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
