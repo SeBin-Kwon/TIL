48824fb32a279fa8634d7770832da62bcac8c4a4
SeBin-Kwon <sbkwon98@gmail.com>
2023-11-02 20:33:39 +0900

    init Alarm project

---
+++ SwiftUI/practice/Alarm/Alarm/Alarm/AlarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlarmApp.swift
+//  Alarm
+//
+//  Created by Sebin Kwon on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI/practice/Alarm/Alarm/Alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Alarm
+//
+//  Created by Sebin Kwon on 11/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
