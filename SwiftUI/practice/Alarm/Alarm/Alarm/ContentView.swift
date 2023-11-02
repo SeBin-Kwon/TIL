f75d485bd19058cb44263ca6f122a5e007238462
SeBin-Kwon <sbkwon98@gmail.com>
2023-11-02 21:32:44 +0900

    Create TabView and MainAlarmView

--- SwiftUI/practice/Alarm/Alarm/Alarm/ContentView.swift
+++ SwiftUI/practice/Alarm/Alarm/Alarm/ContentView.swift
@@ -8,14 +8,34 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var selection = 1
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            TabView(selection: $selection) {
+                Text("세계 시계")
+                    .tabItem {
+                        Image(systemName: "2.square.fill")
+                        Text("세계 시계")
+                    }.tag(0)
+                MainAlarmView(selection: $selection)
+                    .tabItem {
+                        Image(systemName: "1.square.fill")
+                        Text("알람")
+                    }.tag(1)
+                Text("스톱워치")
+                    .tabItem {
+                        Image(systemName: "2.square.fill")
+                        Text("스톱워치")
+                    }.tag(2)
+                Text("타이머")
+                    .tabItem {
+                        Image(systemName: "3.square.fill")
+                        Text("타이머")
+                    }.tag(3)
+            }
+            .navigationTitle(selection == 1 ? "알람" : "")
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ SwiftUI/practice/Alarm/Alarm/Alarm/MainAlarmView.swift
@@ -0,0 +1,18 @@
+//
+//  MainAlarmView.swift
+//  Alarm
+//
+//  Created by Sebin Kwon on 11/2/23.
+//
+
+import SwiftUI
+
+struct MainAlarmView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainAlarmView()
+}
