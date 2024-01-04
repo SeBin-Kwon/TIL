a5f0b5985106760c04a0340b1ead185612cdd666
strong_bean <sbkwon98@gmail.com>
2024-01-04 09:55:29 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 88 ms, Memory: 34048 KB -BaekjoonHub

---
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -0,0 +1,13 @@
+import sys
+from functools import cmp_to_key
+input = sys.stdin.readline
+
+def compare(a, b):
+    str1 = a + b
+    str2 = b + a
+    return (int(str1) > int(str2)) - (int(str1) < int(str2))
+
+n = int(input())
+numbers = input().split()
+numbers.sort(key= cmp_to_key(compare), reverse= True)
+print(str(int("".join(numbers))))
