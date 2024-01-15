f4abe6e56140c3c34fdcc1c4bb0c548142b64595
strong_bean <sbkwon98@gmail.com>
2024-01-15 17:13:27 +0900

    [level Lv.2] Title: 의상, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42578. 의상/의상.py
@@ -0,0 +1,10 @@
+def solution(clothes):
+    answer = 1
+    dic = {}
+    for cloth in clothes:
+        dic[cloth[1]] = dic.get(cloth[1], 0) + 1
+        
+    for key in dic:
+        answer *= dic[key] + 1
+        
+    return answer - 1
