15851493b2b95fa9385c84309a4a8405851daa16
strong_bean <sbkwon98@gmail.com>
2024-03-11 22:26:40 +0900

    [Silver II] Title: N과 M (12), Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,13 @@
+n, m = map(int, input().split())
+nums = sorted(list(set(map(int, input().split()))))
+answer = []
+
+def dfs(idx):
+    if len(answer) == m:
+        print(*answer)
+        return
+    for i in range(idx,len(nums)):
+        answer.append(nums[i])
+        dfs(i)
+        answer.pop()
+dfs(0)
