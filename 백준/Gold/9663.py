ceea38d7fb8ece069de8a1f868498de1397fe2c7
SeBin-Kwon <sbkwon98@gmail.com>
2024-03-15 23:56:36 +0900

    Update Baekjoon

---
+++ 백준/Gold/9663.py
@@ -0,0 +1,27 @@
+import sys
+sys.stdin = open('9663.txt', 'r')
+input = sys.stdin.readline
+
+n = int(input())
+row = [0] * n
+cnt = 0
+
+def attack(x):
+    for i in range(x): 
+        if row[x] == row[i] or abs(row[x] - row[i]) == abs(x - i):
+            return True
+    return False
+ 
+def dfs(start):
+    global cnt
+    if start == n:
+        cnt += 1
+        return
+    else:
+        for i in range(n):
+            row[start] = i
+            if not attack(start):
+                dfs(start + 1)
+ 
+dfs(0)
+print(cnt)
