18726d80faf235fb857b49b15cc34f1d6465ec14
strong_bean <sbkwon98@gmail.com>
2024-04-17 23:50:51 +0900

    [level 2] Title: 타겟 넘버, Time: 4.46 ms, Memory: 10.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -0,0 +1,17 @@
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
