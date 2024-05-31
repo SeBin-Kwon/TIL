a1848ee3d884cce5ae9072cdef1e91b520c41431
strong_bean <sbkwon98@gmail.com>
2024-05-31 19:21:19 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.02 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
+++ 프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.swift
@@ -2,17 +2,6 @@ import Foundation
 
 func solution(_ s:String) -> Bool
 {
-    var ans:Bool = false
-    var pCnt = 0
-    var yCnt = 0
-    
-    for i in s {
-        if String(i).caseInsensitiveCompare("p") == .orderedSame {
-            pCnt += 1
-        } else if String(i).caseInsensitiveCompare("y") == .orderedSame {
-            yCnt += 1
-        }
-    }
-
-    return pCnt == yCnt ? !ans : ans
+    let string = s.lowercased()
+    return string.components(separatedBy: "p").count == string.components(separatedBy: "y").count
 }
