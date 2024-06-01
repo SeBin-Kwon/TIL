5a7e14f9f20ba05e08efc87e47e8f76c73e134ac
strong_bean <sbkwon98@gmail.com>
2024-06-01 20:49:51 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.swift
@@ -1,11 +1,4 @@
 func solution(_ s:String) -> Bool {
     guard s.count == 4 || s.count == 6 else { return false }
-    
-    for i in s {
-        if let c = Int(String(i)) {
-            continue
-        }
-        return false
-    }
-    return true
+    return Int(s) != nil ? true : false
 }
