915ac2f7e494d1fef4c43e48ffdf6e521544d0bc
strong_bean <sbkwon98@gmail.com>
2024-07-26 20:44:33 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.18 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.swift
@@ -1,34 +1,12 @@
-func solution(_ n:Int, _ arr1:[Int], _ arr2:[Int]) -> [String] {
+func solution(_ n: Int, _ arr1: [Int], _ arr2: [Int]) -> [String] {
     var answer: [String] = []
-    var binaryArr1 = [[Character]]()
-    var binaryArr2 = [[Character]]()
-    
-    for i in 0..<n {
-        var num1 = Array(String(arr1[i], radix: 2))
-        var num2 = Array(String(arr2[i], radix: 2))
-        if num1.count < n {
-            let zero = Array(String(repeating: "0", count: n - num1.count))
-            num1 = zero + num1
-        }
-        if num2.count < n {
-            let zero = Array(String(repeating: "0", count: n - num2.count))
-            num2 = zero + num2
-        }
-        binaryArr1.append(num1)
-        binaryArr2.append(num2)
-    }
 
     for i in 0..<n {
-        var row = ""
-        for j in 0..<n {
-            if binaryArr1[i][j] == "0" && binaryArr2[i][j] == "0" {
-                row += " "
-            } else {
-                row += "#"
-            }
-        }
+        let binary = String(arr1[i] | arr2[i], radix: 2)
+        let padded = String(repeating: "0", count: n - binary.count) + binary
+        let row = padded.map { $0 == "0" ? " " : "#" }.joined()
         answer.append(row)
     }
-    
+
     return answer
 }
