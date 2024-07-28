58ffddc13881e05c4182535101c4b7edfabf01c3
SeBin-Kwon <sbkwon98@gmail.com>
2024-07-28 23:53:01 +0900

    Update Swift Timer

--- Swift/practice/Timer/Timer/ViewController.swift
+++ Swift/practice/Timer/Timer/ViewController.swift
@@ -6,14 +6,68 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+     
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var slider: UISlider!
+    weak var timer: Timer?
+    var number = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        configureUI()
+    }
+    
+    func configureUI() {
+        mainLabel.text = "초를 선택하세요"
     }
 
-
+    @IBAction func sliderChanged(_ sender: UISlider) {
+        number = Int(sender.value * 60)
+        mainLabel.text = "\(number) 초"
+    }
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
+        timer?.invalidate()
+//        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] _ in
+//            if number > 0 {
+//                number -= 1
+//                slider.value = Float(number) / Float(60)
+//                mainLabel.text = "\(number) 초"
+//            } else {
+//                number = 0
+//                mainLabel.text = "초를 선택하세요"
+//                timer?.invalidate()
+//                AudioServicesPlayAlertSound(SystemSoundID(1322))
+//            }
+//        }
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomethingAfter1Second), userInfo: nil, repeats: true)
+    }
+    
+    @objc func doSomethingAfter1Second() {
+        if number > 0 {
+            number -= 1
+            slider.value = Float(number) / Float(60)
+            mainLabel.text = "\(number) 초"
+        } else {
+            number = 0
+            mainLabel.text = "초를 선택하세요"
+            timer?.invalidate()
+            AudioServicesPlayAlertSound(SystemSoundID(1322))
+        }
+    }
+    
+    
+    @IBAction func resetButtonTapped(_ sender: UIButton) {
+        mainLabel.text = "초를 선택하세요"
+        slider.value = 0.5
+        number = 0
+        timer?.invalidate()
+//        slider.setValue(0.5, animated: true)
+    }
+    
+    
 }
 
