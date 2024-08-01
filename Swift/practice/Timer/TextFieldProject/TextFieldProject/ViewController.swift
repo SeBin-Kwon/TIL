efd4162d53034cc6473beaabc1a32f52a89e2a0f
SeBin-Kwon <sbkwon98@gmail.com>
2024-08-01 23:40:35 +0900

    Update TextFieldProject

--- Swift/practice/Timer/TextFieldProject/TextFieldProject/ViewController.swift
+++ Swift/practice/Timer/TextFieldProject/TextFieldProject/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setup()
+    }
+    
+    func setup() {
+        view.backgroundColor = UIColor.gray
+        textField.keyboardType = .emailAddress
+        textField.placeholder = "이메일 입력"
+        textField.borderStyle = .roundedRect
+        textField.clearButtonMode = .always
+        textField.returnKeyType = .next
     }
 
-
+    @IBAction func doneButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
