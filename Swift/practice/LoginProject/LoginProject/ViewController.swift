a82fb6e09ba7e0ae44ac2254fe095498ffb8fb49
SeBin-Kwon <sbkwon98@gmail.com>
2024-08-20 22:52:34 +0900

    Update LoginProject

--- Swift/practice/LoginProject/LoginProject/ViewController.swift
+++ Swift/practice/LoginProject/LoginProject/ViewController.swift
@@ -9,7 +9,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let emailTextFieldView = UIView()
+//    let emailTextFieldView = UIView()
+    let emailTextFieldView: UIView = {
+       let view = UIView()
+        view.backgroundColor = UIColor.darkGray
+        view.layer.cornerRadius = 10
+        view.layer.masksToBounds = true
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,9 +24,7 @@ class ViewController: UIViewController {
     }
 
     func makeUI() {
-        emailTextFieldView.backgroundColor = UIColor.darkGray
         view.addSubview(emailTextFieldView)
-        
         emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
         emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
         emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
