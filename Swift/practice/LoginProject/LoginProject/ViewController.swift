06100cb16b9ee3d30d6d2888d3c4ecccf15701aa
SeBin-Kwon <sbkwon98@gmail.com>
2024-08-27 23:27:17 +0900

    Update LoginProject

--- Swift/practice/LoginProject/LoginProject/ViewController.swift
+++ Swift/practice/LoginProject/LoginProject/ViewController.swift
@@ -9,13 +9,77 @@ import UIKit
 
 class ViewController: UIViewController {
 
-//    let emailTextFieldView = UIView()
-    let emailTextFieldView: UIView = {
+    // MARK: - 이메일 입력하는 텍스트 뷰
+    private lazy var emailTextFieldView: UIView = {
        let view = UIView()
         view.backgroundColor = UIColor.darkGray
-        view.layer.cornerRadius = 10
-        view.layer.masksToBounds = true
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
+//        view.addSubview(emailTextField)
         return view
+    }()
+    
+    // 이메일 또는 전화번호 안내문구
+    private var emailInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "이메일 또는 전화번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = UIColor.gray
+        return label
+    }()
+    
+    // 로그인 - 이메일 입력 필드
+    private lazy var emailTextField: UITextField = {
+        var tf = UITextField()
+        tf.frame.size.height = 48
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none
+        tf.autocorrectionType = .no
+        tf.spellCheckingType = .no
+        tf.keyboardType = .emailAddress
+//        tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        return tf
+    }()
+    
+    // MARK: - 비밀번호 입력하는 텍스트 뷰
+    private lazy var passwordTextFieldView: UIView = {
+        let view = UIView()
+        //view.frame.size.height = 48
+        view.backgroundColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        view.layer.cornerRadius = 5
+        view.clipsToBounds = true
+//        view.addSubview(passwordTextField)
+//        view.addSubview(passwordInfoLabel)
+//        view.addSubview(passwordSecureButton)
+        return view
+    }()
+    
+    // 패스워드텍스트필드의 안내문구
+    private let passwordInfoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "비밀번호"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textColor = #colorLiteral(red: 0.8374180198, green: 0.8374378085, blue: 0.8374271393, alpha: 1)
+        return label
+    }()
+    
+    // 로그인 - 비밀번호 입력 필드
+    private lazy var passwordTextField: UITextField = {
+        let tf = UITextField()
+        tf.backgroundColor = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        tf.frame.size.height = 48
+        tf.backgroundColor = .clear
+        tf.textColor = .white
+        tf.tintColor = .white
+        tf.autocapitalizationType = .none
+        tf.autocorrectionType = .no
+        tf.spellCheckingType = .no
+        tf.isSecureTextEntry = true
+        tf.clearsOnBeginEditing = false
+//        tf.addTarget(self, action: #selector(textFieldEditingChanged(_:)), for: .editingChanged)
+        return tf
     }()
     
     override func viewDidLoad() {
@@ -24,12 +88,11 @@ class ViewController: UIViewController {
     }
 
     func makeUI() {
-        view.addSubview(emailTextFieldView)
-        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
-        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
-        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
-        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
-        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+//        emailTextFieldView.translatesAutoresizingMaskIntoConstraints = false
+//        emailTextFieldView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30).isActive = true
+//        emailTextFieldView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30).isActive = true
+//        emailTextFieldView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200).isActive = true
+//        emailTextFieldView.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
 }
