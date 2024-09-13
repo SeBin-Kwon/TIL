5768a9f1b69d0491e9a0bba48a7c8c672d1a3dc2
SeBin-Kwon <sbkwon98@gmail.com>
2024-09-13 23:50:26 +0900

    Add BMI project

---
+++ Swift/practice/BMI/BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by Sebin Kwon on 9/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
