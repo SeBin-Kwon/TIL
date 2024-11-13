6abb47b525d09b40eebc4f1cf0bf3dbf1f0e818b
strong_bean <sbkwon98@gmail.com>
2024-11-13 23:00:20 +0900

    [level 1] Title: 과일 장수, Time: 3096.95 ms, Memory: 60.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.swift
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.swift
@@ -1,21 +1,9 @@
 import Foundation
 
 func solution(_ k:Int, _ m:Int, _ score:[Int]) -> Int {
-    // 내림차순 정렬
-    // m개씩 자르기, 남는건 버리기
-    // 상자마다 min * m 가격구해서 더하기
-    
-    var answer = 0
-    var sortedScore = score.sorted(by:>)
-    // var price = 0
-    var box = [Int]()
-    for i in 0..<sortedScore.count {
-        box.append(sortedScore[i])
-        if box.count == m {
-            answer += box.min()! * m
-            box = [] 
+    let s = score.sorted(by: >)
+    return stride(from: m-1, to: score.count, by: m)
+        .reduce(0) { r, e in
+             r + s[e] * m 
         }
-    }
-    
-    return answer
 }
