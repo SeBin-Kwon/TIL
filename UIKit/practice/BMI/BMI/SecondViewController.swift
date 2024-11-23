90122c8c2d68b2cc2eec946f6d8d90409f5e4b36
SeBin-Kwon <sbkwon98@gmail.com>
2024-11-23 21:14:25 +0900

    Add makeUI, SecondViewController

---
+++ UIKit/practice/BMI/BMI/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMI
+//
+//  Created by Sebin Kwon on 11/23/24.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- UIKit/practice/BMI/BMI/ViewController.swift
+++ UIKit/practice/BMI/BMI/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var heightTextField: UITextField!
+    @IBOutlet weak var weightTextField: UITextField!
+    @IBOutlet weak var calculateButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeUI()
+    }
+    
+    func makeUI() {
+        heightTextField.delegate = self
+        weightTextField.delegate = self
+        mainLabel.text = "키와 몸무게를 입력해주세요"
+        calculateButton.clipsToBounds = true
+        calculateButton.layer.cornerRadius = 5
+        calculateButton.setTitle("BMI 계산하기", for: .normal)
+        heightTextField.placeholder = "cm단위로 입력해주세요"
+        weightTextField.placeholder = "kg단위로 입력해주세요"
     }
 
-
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+    }
+    
 }
 
+extension ViewController: UITextFieldDelegate {
+    
+}
