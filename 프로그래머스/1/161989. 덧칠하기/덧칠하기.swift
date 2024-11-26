12d467b634e3e5afb3274b65e2b00f77a53c89a8
strong_bean <sbkwon98@gmail.com>
2024-11-26 21:01:41 +0900

    [level 1] Title: 덧칠하기, Time: 11.85 ms, Memory: 18 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.swift
@@ -1,22 +1,16 @@
 import Foundation
 
 func solution(_ n:Int, _ m:Int, _ section:[Int]) -> Int {
-    var walls = Array(repeating: false, count: n+1)
-    var answer = 0
-    
+
+    var painted = 0
+    var ans = 0
+
     for s in section {
-        walls[s] = true
-    }
-    
-    for i in 1...n {
-        if walls[i] {
-            for j in i..<i+m {
-                guard j <= n else { break }
-                walls[j] = false
-            }
-            answer += 1
+        if painted < s {
+            painted = s + m - 1
+            ans += 1
         }
     }
-    
-    return answer
+
+    return ans
 }
