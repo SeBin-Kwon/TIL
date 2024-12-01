bae85bdc9a0f7439d75b3f54b0f3e7e9085a0fd6
SeBin-Kwon <sbkwon98@gmail.com>
2024-12-01 15:47:11 +0900

    Update SecondViewController

--- UIKit/practice/BMI/BMI/SecondViewController.swift
+++ UIKit/practice/BMI/BMI/SecondViewController.swift
@@ -9,8 +9,24 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var bmiNumberLabel: UILabel!
+    @IBOutlet weak var adviceLabel: UILabel!
+    @IBOutlet weak var backButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeUI()
+    }
+    
+    func makeUI() {
+        bmiNumberLabel.clipsToBounds = true
+        bmiNumberLabel.layer.cornerRadius = 8
+        bmiNumberLabel.backgroundColor = .gray
+        backButton.clipsToBounds = true
+        backButton.layer.cornerRadius = 5
     }
 
+    @IBAction func backButtonTapped(_ sender: UIButton) {
+        dismiss(animated: true)
+    }
 }
