23386d537b10208471e878a6e3c30a30969ff512
strong_bean <sbkwon98@gmail.com>
2024-12-03 00:02:16 +0900

    [level 1] Title: 둘만의 암호, Time: 0.28 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.swift
@@ -1,23 +1,14 @@
 import Foundation
 
 func solution(_ s:String, _ skip:String, _ index:Int) -> String {
-    let alphabet = "abcdefghijklmnopqrstuvwxyz".map { String($0) }
-    var s = s.map { String($0) }
-    
-    for i in 0..<s.count {
-        if var c = alphabet.firstIndex(of: s[i]) {
-            var index = index
-            
-            while index != 0 {      
-                c += 1
-                c %= 26
-                
-                if skip.contains(alphabet[c]) { continue }
-                
-                index -= 1
-                s[i] = alphabet[c]
-            }
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".filter { !skip.contains($0) }.map { String($0) }
+    let string = s.map { String($0) }
+    var answer = ""
+    for s in string {
+        if let first = alphabet.firstIndex(of: s) { 
+            let num = (first + index) % alphabet.count
+            answer += alphabet[num]
         }
     }
-    return s.joined()
+    return answer
 }
