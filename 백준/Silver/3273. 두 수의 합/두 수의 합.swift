f36e9f9c401c6f65246f900a78957df1c5dbf647
strong_bean <sbkwon98@gmail.com>
2025-03-27 15:49:08 +0900

    [Silver III] Title: 두 수의 합, Time: 44 ms, Memory: 76884 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.swift
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.swift
@@ -8,22 +8,15 @@ var left = 0
 var right = n - 1
 var count = 0
 
-while true {
+while left < right {
     let sumResult = sortedArray[left] + sortedArray[right]
     if x == sumResult {
         count += 1
-        if right - left == 1 {
-            break
-        }
-        right -= 1
         left += 1
     } else if x < sumResult {
         right -= 1
     } else {
         left += 1
-    }
-    if left == right || left > right {
-        break
     }
 }
 
