8e5f9e0680d4196d7d714875d871313407266f67
strong_bean <sbkwon98@gmail.com>
2025-07-15 15:12:29 +0900

    [level 1] Title: 추억 점수, Time: 2.17 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.swift
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.swift
@@ -5,5 +5,6 @@ func solution(_ name:[String], _ yearning:[Int], _ photo:[[String]]) -> [Int] {
     for i in 0..<name.count {
         dict[name[i]] = yearning[i]
     }
+    
     return photo.map { $0.reduce(0) { $0 + (dict[$1] ?? 0) } }
 }
