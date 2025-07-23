0f8159a5a2e7d50480ea63388c034f18ddba9cd4
strong_bean <sbkwon98@gmail.com>
2025-07-23 10:49:58 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 8 ms, Memory: 79512 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.swift
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.swift
@@ -1,26 +1,21 @@
 import Foundation
 
-var list = [Int]()
+var arr = [Int]()
 for _ in 0..<9 {
-    let num = Int(readLine()!)!
-    list.append(num)
+    arr.append(Int(readLine()!)!)
 }
-let remain = list.reduce(0, +) - 100
-var a = 0
-var b = 0
-var flag = false
-for i in 0..<9 {
+var notSeven = (0, 0)
+arr.sort()
+let sumNum = arr.reduce(0, +)
+for i in 0..<9-1 {
     for j in i+1..<9 {
-        if list[i] + list[j] == remain {
-            a = i
-            b = j
-            flag = true
-            break
+        if sumNum - arr[i] - arr[j] == 100 {
+            notSeven = (i, j)
         }
     }
-    if flag { break }
 }
-for num in list.sorted() {
-    if num == list[a] || num == list[b] { continue }
-    print(num)
+
+for i in 0..<9 {
+    if i == notSeven.0 || i == notSeven.1 { continue }
+    print(arr[i])
 }
